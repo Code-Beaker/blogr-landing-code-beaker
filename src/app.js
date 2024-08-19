da8c77f8b8d402c1a71ff70a3654f59243140d73
Tharun Raj <tharun.2.linux@gmail.com>
2024-08-19 08:52:37 +0530

    add aria-label for links

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const navlink = document.querySelectorAll(".navbar__links--link");
-const dropdownIcon = document.querySelectorAll(".navbar__links--link__icon");
+const navlinks = document.querySelectorAll(".navbar__links--link");
+const dropdownIcons = document.querySelectorAll(".navbar__links--link__icon");
 
-for (let i = 0; i <= navlink.length; i++) {
-  navlink[i].addEventListener("click", () => {
-    dropdownIcon[i].classList.toggle("drop-expanded");
+for (let i = 0; i <= navlinks.length; i++) {
+  navlinks[i].addEventListener("click", () => {
+    dropdownIcons[i].classList.toggle("drop-expanded");
   });
 }
